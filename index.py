7afca4f31815b44edc6ec7ac5fb9b9a54addf8ef
mohamed chejjar <mchejjar@yahoo.com>
2024-06-22 12:34:09 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+to achieve 400 total Codewars honor points, you need to accumulate points by solving katas and challenges on the Codewars platform. The honor points are typically awarded based on the difficulty and rank of the katas you complete. Here’s a general breakdown of how you might approach thisChoose katas wisely: Start with easier katas if you're new to Codewars to quickly accumulate points. As you gain more experience and confidence, you can move on to more challenging ones.
+
+1.Solve katas regularly: Consistency is key. Solve katas daily or regularly to steadily increase your honor points.
+
+2.Improve your skills: Focus on learning from the solutions and discussions provided by other users. This will help you improve and solve more complex katas in the future.
+
+3 .Check your progress: Monitor your honor points regularly to see how close you are to reaching 400 points.
+
+4. Submit solutions: Make sure to submit your solutions to katas to earn honor points. The faster and more accurately you solve them, the more points you can earn.
